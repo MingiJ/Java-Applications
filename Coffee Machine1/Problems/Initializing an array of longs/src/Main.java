8fe0dd3cdc6faabb3bc1ebda3723b44cc2e09fb9
MingiJ <jeffawesome.ja@gmail.com>
2020-05-13 16:45:53 +0300

    Coffee Machine

---
+++ Coffee Machine1/Problems/Initializing an array of longs/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        long[3] longNumbers = {100000000001, 100000000002, 100000000003};
+
+        System.out.println(Arrays.toString(longNumbers));
+    }
+}
